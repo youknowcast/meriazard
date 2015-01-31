9596f3cf3f7d4bf0c7dcf31bdeac573ebd2f9152
ambe.hideki <ykc@daycrift.net>
2015-01-31 20:18:14 +0900

    start shell changed.

--- start_server.sh
+++ start_server.sh
@@ -1,20 +1,20 @@
 WORKSPACE=/Users/youknow/workspace/meriazard
 SERVER_PORT=3000
-USE_DEBUGGER=1
+USE_DEBUGGER=0
 # the port where node-inspector worked.
 DEBUGGER_PORT=8080
 
 pushd $WORKSPACE
 # if already server started. lets restart.
 echo "start server.."
-wget --output-document=/dev/null -q --spider localhost:${SERVER_PORT}
+wget --output-document=/dev/null --timeout=100 --tries=1 -q --spider http://localhost:${SERVER_PORT}
 is_active=`echo $?`
 if [ "${is_active}" = "0" ]; then
   pgrep -f "node .* app.js" | xargs kill
 fi
 
 # kill debugger if node-inspector is worked.
-wget --output-document=/dev/null -q --spider localhost:${DEBUGGER_PORT}
+wget --output-document=/dev/null --timeout=100 --tries=1 -q --spider http://localhost:${DEBUGGER_PORT}
 is_debugger_active=`echo $?`
 if [ "${is_debugger_active}" = "0" ]; then
   pgrep -f "node-inspector" | xargs kill
